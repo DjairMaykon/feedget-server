069b2c1272ec266fdd8072841de0b6c49d86256b
djairmaykon <djairmaykon@gmail.com>
2022-05-04 12:54:04 -0300

    feat: envia email com o feedback cadastrado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "1054f090e04f24",
+    pass: "010b9a8f3a9a45",
+  },
+});
 
 app.post("/feedback", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -13,6 +23,18 @@ app.post("/feedback", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equipe FeedGet <oi@feedget.com>",
+    to: "Djair Maykon <djairmaykon@gmail.com>",
+    subject: "Novo Feedback",
+    html: [
+      `<div class="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });
